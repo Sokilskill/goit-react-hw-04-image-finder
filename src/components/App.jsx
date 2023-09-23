4ede3ed309c8b22cb64f4a84b229173700c81ab5
Sokilskill <vprovorotov@gmail.com>
2023-09-23 10:09:04 +0300

    refactoring on hooks App, ImageItem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,23 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 
 import 'react-toastify/dist/ReactToastify.min.css';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handlerFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  handlerFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
-  };
+  return (
+    <>
+      <SearchBar onSubmit={handlerFormSubmit} />
 
-  render() {
-    const { searchQuery } = this.state;
-    return (
-      <>
-        <SearchBar onSubmit={this.handlerFormSubmit} />
-
-        <ImageGallery searchQuery={searchQuery} />
-        <ToastContainer />
-      </>
-    );
-  }
-}
+      <ImageGallery searchQuery={searchQuery} />
+      <ToastContainer />
+    </>
+  );
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,38 +1,33 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal/Modal';
 import css from './ImageGalleryItem.module.css';
 
-class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+const ImageGalleryItem = ({
+  imagePreview: { webformatURL, largeImageURL, tags },
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  console.log('show', showModal);
+  const onToggleModal = () => {
+    setShowModal(state => !state);
   };
 
-  onToggleModal = () => {
-    this.setState(state => ({ showModal: !state.showModal }));
-  };
-
-  render() {
-    const {
-      imagePreview: { webformatURL, largeImageURL, tags },
-    } = this.props;
-    return (
-      <>
-        <li className={css.imageGalleryItem} onClick={this.onToggleModal}>
-          <img
-            src={webformatURL}
-            alt={tags}
-            className={css['imageGalleryItem-image']}
-            loading="lazy"
-          />
-        </li>
-        {this.state.showModal && (
-          <Modal tag={tags} img={largeImageURL} onToggle={this.onToggleModal} />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <li className={css.imageGalleryItem} onClick={onToggleModal}>
+        <img
+          src={webformatURL}
+          alt={tags}
+          className={css['imageGalleryItem-image']}
+          loading="lazy"
+        />
+      </li>
+      {showModal && (
+        <Modal tag={tags} img={largeImageURL} onToggle={onToggleModal} />
+      )}
+    </>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   imagePreview: PropTypes.object.isRequired,
